50839dc5e82e50151c4117d5554e9c193036b1ee
acuervoa <kuerbo@gmail.com>
2020-04-07 20:48:26 +0200

    Programatic navigation

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const express = require("express");
+const express = require('express');
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
