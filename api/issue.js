6b2372f30178fdd8ff2e3cd3023e42cfe0228715
acuervoa <kuerbo@gmail.com>
2020-04-07 20:48:58 +0200

    change schema

--- api/issue.js
+++ api/issue.js
@@ -1,38 +1,40 @@
 const { UserInputError } = require('apollo-server-express');
 const { getDb, getNextSequence } = require('./db.js');
 
-async function list() {
-    const db = getDb();
-    const issues = await db.collection('issues').find({}).toArray();
-    return issues;
+async function list(_, { status }) {
+  const db = getDb();
+  const filter = {};
+  if (status) filter.status = status;
+  const issues = await db.collection('issues').find(filter).toArray();
+  return issues;
 }
 
 function validate(_, { issue }) {
-    const errors = [];
-    if (issue.title.length < 3) {
-      errors.push('Field "title" must be at least 3 characters long.');
-    }
-    if (issue.status === 'Assigned' && !issue.owner) {
-      errors.push('Field "owner" is required when status is "Assigned"');
-    }
-  
-    if (errors.length > 0) {
-      throw new UserInputError('Invalid input(s)', { errors });
-    }
+  const errors = [];
+  if (issue.title.length < 3) {
+    errors.push('Field "title" must be at least 3 characters long.');
   }
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is required when status is "Assigned"');
+  }
+
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors });
+  }
+}
 
 
 async function add(_, { issue }) {
-    const db=getDb();
-    validate('', issue);
-    const newIssue = { ...issue };
-    newIssue.created = new Date();
-    newIssue.id = await getNextSequence('issues');
-    const result = await db.collection('issues').insertOne(newIssue);
-  
-    const savedIssue = await db.collection('issues')
-      .findOne({ _id: result.insertedId });
-    return savedIssue;
-  }
-  
-module.exports = {list, add};
+  const db = getDb();
+  validate('', issue);
+  const newIssue = { ...issue };
+  newIssue.created = new Date();
+  newIssue.id = await getNextSequence('issues');
+  const result = await db.collection('issues').insertOne(newIssue);
+
+  const savedIssue = await db.collection('issues')
+    .findOne({ _id: result.insertedId });
+  return savedIssue;
+}
+
+module.exports = { list, add };
